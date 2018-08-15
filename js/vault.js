8e969885d614f2a6c6d4088338b8d275ec761cad
Clay Ewing <c.ewing@miami.edu>
2018-08-15 15:25:49 -0400

    add featherlight initialization

--- js/vault.js
+++ js/vault.js
@@ -32,27 +32,27 @@ function unlock(item) {
 	var html = "<div class='item'><h3>";
 	switch(item) {
 		case "nih1":
-			html +="NIH Article"
-			html += "<span class='new-label'>New!</span></h3><p>";
-			html +="Article discussing health disparities amongst different races</p>";
-			html +="<a href='#'>View</a>";
+			html +="Racial/Ethnic Differences in Physician Distrust in the United States"
+			html += "<span class='new-label'>Unlocked!</span></h3><p>";
+			html +="Publication from a study funded by the National Institutes of Health</p>";
+			html +="<a href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1913079/' data-featherlight='iframe' data-featherlight-iframe-allowfullscreen>View</a>";
 			break;
 		case "nejm1":
-			html +="NEJM Article"
-			html += "<span class='new-label'>New!</span></h3><p>";
-			html +="Article discussing health disparities amongst different races</p>";
-			html +="<a href='#'>View</a>";
+			html +="Racial Differences in the Use of Cardiac Catheterization after Acute Myocardial Infarction"
+			html += "<span class='new-label'>Unlocked!</span></h3><p>";
+			html +="Severeal studies have reported that black patients are less likely than white patients to undergo cardiac catheterization after acute myocardial Infarction</p>";
+			html +="<a href='http://www.nejm.org/doi/full/10.1056/NEJM200105103441906' data-featherlight='iframe' data-featherlight-iframe-allowfullscreen>View</a>";
 			break;
 		case "newyorker1":
-			html +="New Yorker Article"
-			html += "<span class='new-label'>New!</span></h3><p>";
-			html +="Article discussing health disparities amongst different races</p>";
-			html +="<a href='https://www.newyorker.com/magazine/2018/02/05/what-does-it-mean-to-die'>View</a>";
+			html +="What Does It Mean To Die?"
+			html += "<span class='new-label'>Unlocked!</span></h3><p>";
+			html +="Jahi McMath, a Black 13 year old who went to the doctors for a tonsilectomy ended up braindead after doctors ignored her health histories and pushed her out of the hospital early</p>";
+			html +="<a href='https://www.newyorker.com/magazine/2018/02/05/what-does-it-mean-to-die' data-featherlight='iframe' data-featherlight-iframe-allowfullscreen>View</a>";
 			break;
 		case "michigantoday":
-			html +="Michigan Today Article"
-			html += "<span class='new-label'>New!</span></h3><p>";
-			html +="Article discussing health disparities amongst different races</p>";
+			html +="The Protest Psychosis"
+			html += "<span class='new-label'>Unlocked!</span></h3><p>";
+			html +="How Schizophrenia Became a Black Disease</p>";
 			html +="<a href='http://michigantoday.umich.edu/a7776/'>View</a>";
 			break;
 		case "finished_injury":
@@ -69,5 +69,6 @@ function unlock(item) {
 	if(!vaultOpen) {
 		openVault();
 	}
+	new_item.children("a").featherlight();
 	$(".item .new-label").fadeIn(500).delay(3000).fadeOut(3000);
 }
