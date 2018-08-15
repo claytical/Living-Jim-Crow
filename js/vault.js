fd191556d8a3be623a040afdd9e4c236944df485
Clay Ewing <c.ewing@miami.edu>
2018-08-15 16:33:46 -0400

    adding image for lightbox test

--- js/vault.js
+++ js/vault.js
@@ -46,20 +46,25 @@ function unlock(item) {
 			html +="Coquetting with Beckly"
 			html += "<span class='new-label'>Unlocked!</span></h3><p>";
 			html +="Nimick Again Tempting the First Baseman to Abandon Players</p>";
-			html +="<a href='vault/articles/union.pdf'>View</a>";
+			html +="<a class='pure-button' href='vault/articles/union.pdf'>View</a>";
 
 			break;
 		case "newyorker1":
 			html +="What Does It Mean To Die?"
 			html += "<span class='new-label'>Unlocked!</span></h3><p>";
 			html +="Jahi McMath, a Black 13 year old who went to the doctors for a tonsilectomy ended up braindead after doctors ignored her health histories and pushed her out of the hospital early</p>";
-			html +="<a href='https://www.newyorker.com/magazine/2018/02/05/what-does-it-mean-to-die' data-featherlight='iframe' data-featherlight-iframe-allowfullscreen>View</a>";
+			html +="<a class='pure-button' href='https://www.newyorker.com/magazine/2018/02/05/what-does-it-mean-to-die' data-featherlight='iframe' data-featherlight-iframe-allowfullscreen>View</a>";
 			break;
 		case "michigantoday":
-			html +="The Protest Psychosis"
+/*			html +="The Protest Psychosis"
 			html += "<span class='new-label'>Unlocked!</span></h3><p>";
 			html +="How Schizophrenia Became a Black Disease</p>";
 			html +="<a href='http://michigantoday.umich.edu/a7776/'>View</a>";
+*/
+			html +="Assaultive and belligerent?"
+			html += "<span class='new-label'>Unlocked!</span></h3><p>";
+			html +="How Schizophrenia Became a Black Disease</p>";
+			html +="<a class='pure-button' href='https://thesocietypages.org/socimages/files/2011/05/1.png'>View</a>";
 			break;
 		case "finished_injury":
 			html +="Injury Story Complete"
